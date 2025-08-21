5050ea686e6384ad8ddeaa01d83aaaab8e58a78e
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-08-21 14:47:45 +0300

    Refactor Onboarding1 layout and adjust image positioning for improved UI

--- app/(onboarding)/onboarding1.tsx
+++ app/(onboarding)/onboarding1.tsx
@@ -18,7 +18,7 @@ export default function Onboarding1() {
     >
       <View className="flex-1 justify-center">
         {/* Header Section */}
-        <View className="mb-8 mt-3 mx-6">
+        <View className="mb-3 mt-3 mx-6">
           <View className="relative">
             <Text className="text-4xl font-medium text-gray-900 tracking-tight">
               Take a photo to <Text className="font-extrabold">identify</Text>
@@ -28,18 +28,14 @@ export default function Onboarding1() {
             {/* Çizgi Asset'i */}
             <Image
               source={require("../../assets/plantassets/identify.png")} // Çizgi asset'iniz
-              className="absolute"
-              style={{
-                top: 32,
-                left: 200,
-              }}
+              className="absolute top-8 right-5"
               resizeMode="contain"
             />
           </View>
         </View>
 
         {/* Main Image */}
-        <View className="flex-1 items-center">
+        <View className="flex-1 items-center mt-3">
           <Image
             source={require("../../assets/plantassets/Content.png")}
             resizeMode="contain"
@@ -48,10 +44,12 @@ export default function Onboarding1() {
         </View>
 
         {/* Bottom Section */}
-        <View className="mb-10 mx-6">
+        <View className="mx-6 mb-5">
           <OnboardingButton title="Continue" onPress={handleContinue} />
+        </View>
 
-          {/*  */}
+        {/* Pagination at the very bottom */}
+        <View className="mx-6 mb-3">
           <OnboardingPagination totalSteps={3} currentStep={0} />
         </View>
       </View>
